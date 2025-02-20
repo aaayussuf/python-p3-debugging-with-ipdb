a32dcd24e69a8c7755f86991f1fce747cc5d1285
Abdullahi Abdikadir <aabdikadir849@gmail.com>
2025-02-20 15:33:45 +0300

    fixed the error of python-p3-debugging-with-ipdb

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -2,5 +2,5 @@ import ipdb
 
 def plus_two(num):
     num = num + 2  # Fixing the issue
-    ipdb.set_trace()  # Debugging breakpoint
+    # ipdb.set_trace()  # Remove or comment this out
     return num
